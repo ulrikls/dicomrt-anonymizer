ccf851548539e8b8f27bb97c4c5bb8e6fbf7baea
jenstolesen <jtol07@student.aau.dk>
2012-02-21 13:23:45 +0100

    Included comments on code

--- anonymizer.m
+++ anonymizer.m
@@ -1,10 +1,27 @@
+%% ANONYMIZER
+%
+% Anonymizes DICOM-RT files while keeping the contour structeres connected
+% to the individual slices in which they occur.
+%
+% Developed by Ulrik L. Stephansen & Jens T. Olesen
+%
+%%
+
+
+% Generate new Instance ID's for the dicom series
 
 values.StudyInstanceUID = dicomuid;
 values.SeriesInstanceUID = dicomuid;
 
+% Locate all dicom files
+
 d = dir('*.dcm');
 
+% Load the radiotherapy planning structure
+
 rtplanning = dicominfo('rtss.dcm');
+
+% Load all dicom files in the directory and extract SOP id's
 
 for p = 1:numel(d)
  
@@ -12,18 +29,28 @@ for p = 1:numel(d)
  currentSOPClassUID = currentInfo.SOPClassUID;
  currentSOPInstanceUID = currentInfo.SOPInstanceUID;
  
+ % Create new filename for anonymized dicom files
+ 
  [~, name,~] = fileparts(d(p).name);
  newName = sprintf('anon.%s.dcm', name);
  
+ % Anonymize dicom data using built-in matlab function
+ 
  dicomanon(d(p).name, newName,'update', values)
+ 
+ % Retrieve the new SOP id's
  
  updatedInfo = dicominfo(newName);
  updatedSOPClassUID = updatedInfo.SOPClassUID;
  updatedSOPInstanceUID = updatedInfo.SOPInstanceUID;
  
+ % Replace the old SOP id's in the planning structure with the new SOP id's
+ 
  rtplanning = searchstruct(rtplanning, currentSOPClassUID, updatedSOPClassUID);
  rtplanning = searchstruct(rtplanning, currentSOPInstanceUID, updatedSOPInstanceUID);
   
 end
+
+% Save planning structure to dicom file
 
 dicomwrite(dicomread('rtss.dcm'),'anon.rtss.dcm',rtplanning,'CreateMode', 'copy')
